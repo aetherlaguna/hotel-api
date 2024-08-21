d97896b48923331a0a4b1ebd683d70edea36a93a
æstral <73630879+aetherlaguna@users.noreply.github.com>
2024-08-21 10:40:33 -0300

    init: app initialization

---
+++ src/test/java/com/laguna/hotel_api/HotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laguna.hotel_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
