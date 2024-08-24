f8e55376c774d66433a96577c6b3df1b2d34340e
æstral <73630879+aetherlaguna@users.noreply.github.com>
2024-08-24 17:16:26 -0300

    feat: implementing the in memory temporary database

---
+++ src/main/java/com/laguna/hotel_api/adapters/persistence/DataBaseInMemory.java
@@ -0,0 +1,11 @@
+package com.laguna.hotel_api.adapters.persistence;
+
+import com.laguna.hotel_api.domain.entity.Hotel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBaseInMemory {
+
+    public static List<Hotel> hotelDataBase = new ArrayList<>();
+}
